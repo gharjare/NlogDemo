0a58983038e850992774e8384a5b2323bdd1575c
gharjare <gharjarechetan@gmail.com>
2023-02-03 14:40:14 +0530

    [chetan gharjare]initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using NLog;
+
+namespace NLogDemo1
+{
+    class program
+    { 
+        static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main()
+        {
+            Console.WriteLine("Enter number");
+            int number1 = int.Parse(Console.ReadLine());
+            logger.Trace($"User entered first value{number1}");
+            Console.WriteLine("Enter number");
+            int number2 = int.Parse(Console.ReadLine());
+            logger.Trace($"User entered second value{number2}");
+            NLogDemo1.Math m = new NLogDemo1.Math();
+            int result = m.Add(number1, number2);
+            Console.WriteLine($"Result is{result}");
+            int result2 = m.Divide(number2, number1);
+            Console.WriteLine($"Result is{result2}");
+
+        }
+    }
+}
